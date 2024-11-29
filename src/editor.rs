758981a13111d48bc6cab1bdba2d72271f95c776
k4zzzz <76569967+k4zzzz@users.noreply.github.com>
2024-11-29 14:48:20 -0500

    ch3 right before 'clear the screen'

--- src/editor.rs
+++ src/editor.rs
@@ -1,35 +1,50 @@
-use crossterm::event;
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor{}
+        Editor { 
+            should_quit: false
+        }
     }
     
 
-    pub fn run(&self){
-        enable_raw_mode().unwrap();
-        loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match (event.code) {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),   
-                    }   
-                },
-                Err(err) => println!("Error: {}", err),
-                _ => ()
-            }
+    pub fn run(&mut self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
         }
-        disable_raw_mode().unwrap(); 
+        print!("Goodbye.\r\n");
     }
-}
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        loop {
+            if let Key(KeyEvent {
+                code, 
+                modifiers, 
+                kind, 
+                state }) = read()? {
+                    println!("Code: {code:?}
+                    Modifiers: {modifiers:?}
+                    Kind: {kind:?} State: {state:?} \r");
+
+                    match code {
+                        Char('q') if modifiers == KeyModifiers::CONTROL => {
+                            self.should_quit = true;
+                        }
+                        _ =>(),   
+                    }
+                }
+                if self.should_quit {
+                    break;
+                }
+            }
+            disable_raw_mode()?;
+            Ok(())
+        }
+    }
